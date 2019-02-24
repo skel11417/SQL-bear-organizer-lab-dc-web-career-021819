a89e1e3a376e995ed9eb42c0c7cad394615a408c
Sean <shonkelli@gmail.com>
2019-02-23 21:31:17 -0500

    insert.sql

---
+++ lib/insert.sql
@@ -0,0 +1,70 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Mr. Chocolate",
+  3,
+  "m",
+  "white",
+  "sweet",
+  1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Rowdy",
+  2,
+  "m",
+  "brown",
+  "rowdy",
+  1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Tabitha",
+  5,
+  "f",
+  "black",
+  "vicious",
+  1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Sergeant Brown",
+  10,
+  "m",
+  "olive drab",
+  "stately",
+  1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Melissa",
+  8,
+  "f",
+  "calico",
+  "sweet",
+  1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Grinch",
+  5,
+  "m",
+  "brown",
+  "generous",
+  0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  "Wendy",
+  9,
+  "f",
+  "white",
+  "vague",
+  1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (
+  NULL,
+  NULL,
+  "m",
+  "black and white and red all over",
+  "dead",
+  0);
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
